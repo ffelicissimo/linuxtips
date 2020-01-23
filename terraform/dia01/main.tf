d3acc1c63e4b23a880e40527b94ff88191c6f52e
Fernando Felicissimo <ffelicissiom@gmail.com>
2020-01-23 10:54:08 -0300

    Subindo os arquivos da aula 01

---
+++ terraform/dia01/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
+    bucket = "iaasweek-tfstates-terraform"
+    key    = "terraform-test.tfstate"
+    region = "us-east-1"
+  }
+}
