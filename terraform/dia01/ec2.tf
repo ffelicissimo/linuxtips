b24f061f00649f78455931742735d34b33c2710c
Fernando Felicissimo <ffelicissiom@gmail.com>
2020-02-15 12:09:48 -0300

    atualizando repo

--- terraform/dia01/ec2.tf
+++ terraform/dia01/ec2.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["IaaSWeek-${var.hash_commit}"]
   }
 
@@ -10,10 +10,22 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+    #ami = "id da ami"
+    ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-
+  subnet_id = "subnet-e12a3ea7"
+  
   tags = {
     Name = "HelloWorld"
   }
 }
+
+#resource "aws_instance" "west" {
+#    provider = aws.west
+#  ami           = "ami-0e9aa7ba086c3544b"
+#  instance_type = "t2.micro"
+  
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
